676f5eb79324f70073f270f466d41f2f53a7d7f9
Ben Conway <benrconway84@gmail.com>
2020-10-07 20:45:05 +0100

    Updated tests to appear within describe blocks

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -22,7 +22,7 @@ const testIntervals = [
     "2020-01-01T02:15:00.000+02:00/2020-01-01T13:30:00.000+12:00"
   ),
 ];
-const testArray = [
+const testUserArray = [
   {
     id: 1,
     intervals: [testIntervals[0], testIntervals[1]],
@@ -46,7 +46,7 @@ describe("prepareUserArray tests", () => {
 
 describe("startOfEarliestIntervalInUTCString tests", () => {
   test("Can find the start of the earliest interval from a given array of users with intervals", () => {
-    expect(startOfEarliestIntervalInUTCString(testArray)).toBe(
+    expect(startOfEarliestIntervalInUTCString(testUserArray)).toBe(
       "2020-01-01T00:15:00.000Z"
     );
   });
@@ -54,7 +54,7 @@ describe("startOfEarliestIntervalInUTCString tests", () => {
 
 describe("endOfLatestIntervalInUTCString tests", () => {
   test("Can find the end of the latest interval from of a given array of users with intervals", () => {
-    expect(endOfLatestIntervalInUTCString(testArray)).toBe(
+    expect(endOfLatestIntervalInUTCString(testUserArray)).toBe(
       "2020-01-01T04:30:00.000Z"
     );
   });
