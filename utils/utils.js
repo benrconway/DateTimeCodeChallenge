e1e665a9bdb13b669c8dc1425dabac07b2018982
Ben Conway <benrconway84@gmail.com>
2020-10-07 21:04:42 +0100

    Added basic return for lack of supplied path

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 const readFromFile = (pathToFile) => {
+  if (!pathToFile) return "No path supplied";
   return fs.readFileSync(pathToFile, "utf8");
 };
 
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -4,4 +4,7 @@ describe("readFromFile tests", () => {
   test("Can read from testInput and return a string", () => {
     expect(readFromFile("testInput.txt")).toBeTruthy();
   });
+  test("returns a string if no path supplied", () => {
+    expect(readFromFile(null)).toBe("No path supplied");
+  });
 });
