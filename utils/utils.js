17650031f56e86b23a511b8c1251f323f995eeeb
Ben Conway <benrconway84@gmail.com>
2020-10-07 21:12:45 +0100

    Added function to prepare user information into a desired format

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,41 @@
 const fs = require("fs");
+const { Interval } = require("luxon");
 
 const readFromFile = (pathToFile) => {
   if (!pathToFile) return "No path supplied";
   return fs.readFileSync(pathToFile, "utf8");
 };
 
-module.exports = { readFromFile };
+const prepareIntervalfromISO = (intervalISOString) => {
+  // format the interval to be returned
+  return Interval.fromISO(intervalISOString);
+};
+
+const prepareUserArray = (userInformation) => {
+  // split user information into lines
+  const initialSplit = userInformation.split(/[\n]/);
+
+  // map over array of lines to prepare an array of individual users
+  return initialSplit.map((line) => {
+    const intervals = [];
+    // seperate the id from the intervals
+    const seperatedIdAndIntervals = line.split("@");
+    // check there are no undefined results from stray lines etc
+    if (seperatedIdAndIntervals[1] !== undefined) {
+      // seperate individual intervals
+      const intervalsStrings = seperatedIdAndIntervals[1].split(/([\\[\\,\]])/);
+      intervalsStrings.forEach((interval) => {
+        if (interval.length > 1) {
+          intervals.push(prepareIntervalfromISO(interval));
+        }
+      });
+    }
+
+    return {
+      id: seperatedIdAndIntervals[0],
+      intervals,
+    };
+  });
+};
+
+module.exports = { readFromFile, prepareUserArray };
