d05e2ea7408fb71239cb2acae146ba40159a229a
Ben Conway <benrconway84@gmail.com>
2020-10-08 10:14:00 +0100

    Added prepareUserArray to script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // This file will process the supplied input (if valid path is given), and console log the output of the three questions
+const { prepareUserArray } = require("./utils");
 
 const parsedFile = require("yargs").coerce("file", function (arg) {
   return require("fs").readFileSync(arg, "utf8");
 }).argv;
 
-console.log(parsedFile.file);
+const userArray = prepareUserArray(parsedFile.file);
+console.log(userArray);
