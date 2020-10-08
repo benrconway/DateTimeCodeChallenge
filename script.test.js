4fea5c696e68b7ce93575c4e9a3caafef7f4592b
Ben Conway <benrconway84@gmail.com>
2020-10-08 11:31:55 +0100

    Added last test to script.test.js

--- script.test.js
+++ script.test.js
@@ -4,6 +4,7 @@ const {
   prepareUserArray,
   startOfEarliestIntervalInUTCString,
   endOfLatestIntervalInUTCString,
+  supplyArrayOfWorkerOverlappingHours,
 } = require("./utils");
 
 describe("Can correctly answer the following questions:", () => {
@@ -20,6 +21,17 @@ describe("Can correctly answer the following questions:", () => {
   test("What is the ending date and time (in UTC) of the latest interval where any of the workers are free?", () => {
     expect(endOfLatestIntervalInUTCString(userArray)).toBe(
       "2020-01-01T04:45:00.000Z"
+    );
+  });
+  test("What are the intervals of date and times (in UTC) where there are at least 2 workers free?", () => {
+    // without specific guidance I have chosen to store the expected output as an array.
+    // arrays have been converted to strings for simplicity of test comparison
+    expect(supplyArrayOfWorkerOverlappingHours(userArray).toString()).toBe(
+      [
+        "2020-01-01T00:15:00.000Z/2020-01-01T01:30:00.000Z",
+        "2020-01-01T02:00:00.000Z/2020-01-01T03:30:00.000Z",
+        "2020-01-01T04:00:00.000Z/2020-01-01T04:30:00.000Z",
+      ].toString()
     );
   });
 });
