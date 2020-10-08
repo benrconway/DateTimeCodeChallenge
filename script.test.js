4da3bb781f0b484a598de9886d3629ec92c44da5
Ben Conway <benrconway84@gmail.com>
2020-10-08 10:44:57 +0100

    Added first of the three questions to be answered to script.test.js

--- script.test.js
+++ script.test.js
@@ -1,5 +1,20 @@
 // This file will run the tests as supplied by the code challenge against the file 'testInputs.txt'
+const fs = require("fs");
+const {
+  prepareUserArray,
+  startOfEarliestIntervalInUTCString,
+  endOfLatestIntervalInUTCString,
+} = require("./utils");
 
-test("Will hold tests soon", () => {
-  expect(true).toBeTruthy();
+describe("Can correctly answer the following questions:", () => {
+  let userArray;
+  beforeAll(() => {
+    const parsedFile = fs.readFileSync("testInput.txt", "utf8");
+    userArray = prepareUserArray(parsedFile);
+  });
+  test("What is the starting date and time (in UTC) of the earliest interval where any of the workers are free?", () => {
+    expect(startOfEarliestIntervalInUTCString(userArray)).toBe(
+      "2020-01-01T00:15:00.000Z"
+    );
+  });
 });
