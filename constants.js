62e9e7aa508e146fcee91ad3897746a8631c2c93
Ben Conway <benrconway84@gmail.com>
2020-10-12 06:35:28 +0100

    Added string constants for questions

---
+++ constants.js
@@ -0,0 +1,10 @@
+const constants = {
+  QUESTION_ONE:
+    "What is the starting date and time (in UTC) of the earliest interval where any of the workers are free?",
+  QUESTION_TWO:
+    "What is the ending date and time (in UTC) of the latest interval where any of the workers are free?",
+  QUESTION_THREE:
+    "What are the intervals of date and times (in UTC) where there are at least 2 workers free?",
+};
+
+module.exports = constants;
